2b7f087acedfc329b5a567ecce1238b708c2ca19
lcoinsin <coinsin.l@outlook.com>
2024-03-19 23:01:50 +0100

    test : Header 2

--- Frontend/src/components/Header/index.tsx
+++ Frontend/src/components/Header/index.tsx
@@ -8,7 +8,37 @@ interface HeaderProps {
 }
 
 function Header({ headerSection }: HeaderProps) {
-  console.log("headerSection", headerSection);
+  const { i18n } = useTranslation();
+  const [open, setOpen] = useState(false);
+  const [activeLangage, setActiveLangage] = useState(i18n.language);
+
+  const languages = [
+    {
+      label: "FR",
+      code: "fr",
+      flag: <FrenchFlag className="h-5 rounded-full" />,
+    },
+    {
+      label: "EN",
+      code: "en",
+      flag: <EnglishFlag className="h-5 rounded-full" />,
+    },
+  ];
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (!target.closest(".text-footnote")) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <header className="text-small mx-3 flex h-16 items-center justify-between">
@@ -28,7 +58,38 @@ function Header({ headerSection }: HeaderProps) {
       </div>
       {/* Select langage */}
       <div>
-        <LangageTest />
+        <div className="text-footnote">
+          <button
+            className="flex flex-row items-center gap-2 px-4"
+            onClick={() => setOpen(!open)}
+          >
+            {languages.find((lang) => lang.code === activeLangage)?.flag}
+            {languages.find((lang) => lang.code === activeLangage)?.label}
+          </button>
+          {open ? (
+            <div
+              className="absolute mt-2 w-auto rounded-md border border-gray-100 bg-white shadow-lg"
+              role="menu"
+            >
+              {languages
+                .filter((lang) => lang.code !== activeLangage)
+                .map((lang, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-row items-center gap-2 px-4 py-2 duration-200 hover:bg-slate-100"
+                    onClick={() => {
+                      i18n.changeLanguage(lang.code);
+                      setActiveLangage(lang.code);
+                      setOpen(false);
+                    }}
+                  >
+                    {lang.flag}
+                    {lang.label}
+                  </div>
+                ))}
+            </div>
+          ) : null}
+        </div>
       </div>
     </header>
   );
