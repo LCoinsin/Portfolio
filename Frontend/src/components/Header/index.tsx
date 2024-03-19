0253c722bcdc89daf5588727c83eebcb53f3cf20
lcoinsin <coinsin.l@outlook.com>
2024-03-19 23:52:42 +0100

    test : Header 7

--- Frontend/src/components/Header/index.tsx
+++ Frontend/src/components/Header/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import FrenchFlag from "../../assets/images/FrenchFlag";
-import EnglishFlag from "../../assets/images/EnglishFlag";
+import { SVGProps } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-scroll";
 
@@ -70,21 +69,6 @@ function LangageTest() {
 
   return (
     <div className="text-footnote">
-      <div>
-        <svg viewBox="0 0 512 512" className="h-5 rounded-full">
-          <path fill="#012169" d="M0 0h512v512H0z" />
-          <path
-            fill="#FFF"
-            d="M512 0v64L322 256l190 187v69h-67L254 324 68 512H0v-68l186-187L0 74V0h62l192 188L440 0z"
-          />
-          <path
-            fill="#C8102E"
-            d="m184 324 11 34L42 512H0v-3zm124-12 54 8 150 147v45zM512 0 320 196l-4-44L466 0zM0 1l193 189-59-8L0 49z"
-          />
-          <path fill="#FFF" d="M176 0v512h160V0zM0 176v160h512V176z" />
-          <path fill="#C8102E" d="M0 208v96h512v-96zM208 0v512h96V0z" />
-        </svg>
-      </div>
       <button
         className="flex flex-row items-center gap-2 px-4"
         onClick={() => setOpen(!open)}
@@ -118,3 +102,27 @@ function LangageTest() {
     </div>
   );
 }
+
+const FrenchFlag = (props: SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 512 512" {...props}>
+    <path fill="#fff" d="M0 0h512v512H0z" />
+    <path fill="#000091" d="M0 0h170.7v512H0z" />
+    <path fill="#e1000f" d="M341.3 0H512v512H341.3z" />
+  </svg>
+);
+
+const EnglishFlag = (props: SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 512 512" {...props}>
+    <path fill="#012169" d="M0 0h512v512H0z" />
+    <path
+      fill="#FFF"
+      d="M512 0v64L322 256l190 187v69h-67L254 324 68 512H0v-68l186-187L0 74V0h62l192 188L440 0z"
+    />
+    <path
+      fill="#C8102E"
+      d="m184 324 11 34L42 512H0v-3zm124-12 54 8 150 147v45zM512 0 320 196l-4-44L466 0zM0 1l193 189-59-8L0 49z"
+    />
+    <path fill="#FFF" d="M176 0v512h160V0zM0 176v160h512V176z" />
+    <path fill="#C8102E" d="M0 208v96h512v-96zM208 0v512h96V0z" />
+  </svg>
+);
