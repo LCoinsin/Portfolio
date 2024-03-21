b48fbac91127a8f9f1cbb5c8ef5b5509b08ec3bb
lcoinsin <coinsin.l@outlook.com>
2024-03-21 20:23:38 +0100

    Add react-icons package and update contact information

--- Frontend/src/components/Contact/index.tsx
+++ Frontend/src/components/Contact/index.tsx
@@ -1,5 +1,7 @@
-import { LinkedinIcon, Mail, Phone } from "lucide-react";
 import { useTranslation } from "react-i18next";
+import { MdEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaPhone } from "react-icons/fa6";
 
 function Contact({ id }: { id: string }) {
   const { t } = useTranslation();
@@ -11,6 +13,36 @@ function Contact({ id }: { id: string }) {
         <span className="text-primary">.</span>
       </h1>
       <div className="h-[60px]" />
+      <div className="grid grid-cols-1 lg:grid-cols-3">
+        <div className="col-span-1 flex flex-col justify-center gap-y-2 px-2 pr-8">
+          <MdEmail size={30} />
+          <h3 className="text-small font-normal">Email</h3>
+          {/* <p className="text-footnote font-extralight">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, esse
+            nam
+          </p> */}
+          <p className="text-paragraphe font-light">coinsin.l@outlook.com</p>
+        </div>
+        <div className="col-span-1 flex flex-col justify-center gap-y-2 px-2 pr-8">
+          <FaPhone size={30} />
+          <h3 className="text-small font-normal">{t("contact_phone_title")}</h3>
+          {/* <p className="text-footnote font-extralight">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, esse
+            nam
+          </p> */}
+          <p className="text-paragraphe font-light">+33 6 38 04 20 59</p>
+        </div>
+        <div className="col-span-1 flex flex-col justify-center gap-y-2 px-2 pr-8">
+          <FaLinkedin size={30} />
+          <h3 className="text-small font-normal">LinkedIn</h3>
+          {/* <p className="text-footnote font-extralight">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, esse
+            nam
+          </p> */}
+          <p className="text-paragraphe font-light">@leo-coinsin</p>
+        </div>
+      </div>
+      {/* <div className="h-[60px]" />
       <div className="flex flex-col gap-5">
         <div className="text-paragraphe flex flex-row items-center gap-4 font-extralight">
           <div className="flex h-[40pt] w-[40pt] items-center justify-center rounded-full border border-black">
@@ -30,7 +62,7 @@ function Contact({ id }: { id: string }) {
           </div>
           leo-coinsin
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
