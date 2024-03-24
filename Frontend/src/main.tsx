bc5b4e0d2975a3268a1993f857bf98048b94a8e1
lcoinsin <coinsin.l@outlook.com>
2024-03-24 18:47:50 +0100

    Update SpeedInsights import in main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,13 +2,13 @@ import ReactDOM from "react-dom/client";
 import "./i18n";
 import "./index.css";
 import Portfolio from "./pages/Home";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <>
     <Portfolio />
     <SpeedInsights />
-  </>,
+  </>
   // </React.StrictMode>
 );
