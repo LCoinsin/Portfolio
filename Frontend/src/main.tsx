66a711fab37fd08414f2a48a3ccf969955db23e4
lcoinsin <coinsin.l@outlook.com>
2024-03-24 18:51:52 +0100

    Add @vercel/analytics package and import Analytics component

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,12 +3,12 @@ import "./i18n";
 import "./index.css";
 import Portfolio from "./pages/Home";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
   <>
     <Portfolio />
+    <Analytics />
     <SpeedInsights />
   </>
-  // </React.StrictMode>
 );
